f5b424dd8405c3bb964e5a316feca0f0d0ce0b57
Koosha Owji <koosha.owji@gmail.com>
2025-06-19 22:30:55 +1000

    deploy test

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    // Ignore expo-secure-store module that's not needed in Next.js
+    config.externals = config.externals || [];
+    config.externals.push('expo-secure-store');
+    
+    return config;
+  },
+};
 
 module.exports = nextConfig;
