d61d6af61aa124d3e4ac6f7fd744853602007c50
Koosha Owji <koosha.owji@gmail.com>
2025-06-19 20:32:09 +1000

    deploy

---
+++ src/middleware.ts
@@ -0,0 +1,19 @@
+import {
+  withAuth,
+} from "@kinde-oss/kinde-auth-nextjs/middleware";
+import { NextRequest } from "next/server";
+
+export default withAuth(
+  async function middleware(req: NextRequest) {
+  },
+  {
+    publicPaths: ["/", "/api/public"],
+  }
+);
+
+export const config = {
+  matcher: [
+    // Run on everything but Next internals and static files
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)"
+  ],
+};
