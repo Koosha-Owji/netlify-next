e9ce7c15057e82c22a7ae50a85a5abccca8ed207
Koosha Owji <koosha.owji@gmail.com>
2025-06-20 12:54:52 +1000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,13 +4,6 @@ const nextConfig = {
       // enable Node.js–style middleware support
       nodeMiddleware: true,
     },
-    webpack: (config, { isServer }) => {
-      // Ignore expo-secure-store module that's not needed in Next.js
-      config.externals = config.externals || [];
-      config.externals.push('expo-secure-store');
-      
-      return config;
-    },
     // (you can keep any other flags you already have here)
   }
   
