92eeff57bc7f9842e81c1c9e2e39219dedb6978b
Koosha Owji <koosha.owji@gmail.com>
2025-06-20 18:10:06 +1000

    Revert "Update middleware.ts"
    This reverts commit 3160e2065b33e0fe90372e75b80447b8d4217533.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,9 @@
 import {
   withAuth,
 } from "@kinde-oss/kinde-auth-nextjs/middleware";
-import { NextRequest } from "next/server";
 
 export default withAuth(
-  async function middleware(req: NextRequest) {
+  async function middleware(req) {
   },
   {
     publicPaths: ["/", "/api/public"],
