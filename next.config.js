d894c0112429c9f6b0d974ff2059906ffcbb5e46
Koosha Owji <koosha.owji@gmail.com>
2025-06-20 12:48:32 +1000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    experimental: {
-      // enable Node.js–style middleware support
-      nodeMiddleware: true,
-    },
-    // (you can keep any other flags you already have here)
-  }
-  
-  module.exports = nextConfig
-  
+const nextConfig = {};
+
+module.exports = nextConfig;
