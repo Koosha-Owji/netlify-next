ace32c58e9915c02a360842dbacd75d2e338af68
Koosha Owji <koosha.owji@gmail.com>
2025-06-20 14:43:17 +1000

    final test making sure

---
+++ src/middleware.ts
@@ -0,0 +1,19 @@
+import {
+  withAuth,
+} from "@kinde-oss/kinde-auth-nextjs/middleware";
+
+export default withAuth(
+  async function middleware(req) {
+  },
+  {
+    publicPaths: ["/", "/api/public"],
+  }
+);
+
+export const config = {
+  runtime: "nodejs",
+  matcher: [
+    // Run on everything but Next internals and static files
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)"
+  ],
+};
