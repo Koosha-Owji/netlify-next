f6fb04d019c160b55ae2c56d82dae43f1f68ee78
Koosha Owji <koosha.owji@gmail.com>
2025-06-20 10:19:56 +1000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config, { isServer }) => {
-    // Ignore expo-secure-store module that's not needed in Next.js
-    config.externals = config.externals || [];
-    config.externals.push('expo-secure-store');
-    
-    return config;
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
