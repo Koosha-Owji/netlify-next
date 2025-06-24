d8e96dbe878b38d6ca7d929d9a56b2d85861e5f1
Koosha Owji <koosha.owji@gmail.com>
2025-06-24 11:01:16 +1000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,16 +3,6 @@ const nextConfig = {
   experimental: {
     esmExternals: 'loose',
   },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      // Force the ES Module version of Kinde SDK
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        '@kinde-oss/kinde-typescript-sdk': '@kinde-oss/kinde-typescript-sdk/dist/index.js'
-      };
-    }
-    return config;
-  },
 };
 
 module.exports = nextConfig;
