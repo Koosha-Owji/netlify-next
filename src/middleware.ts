a54723c1974247ac9a30362c25ec9f17fdd21fc1
Koosha Owji <koosha.owji@gmail.com>
2025-07-03 18:34:08 +1000

    Testing Regex

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { withAuth } from "@kinde-oss/kinde-auth-nextjs/middleware";
 
 export default withAuth(async function middleware() {}, {
-  publicPaths: ["/", "/api/public", "/api/auth"],
+  publicPaths: ["/api/auth", /^(?!\/dashboard).*$/],
 });
 
 export const config = {
