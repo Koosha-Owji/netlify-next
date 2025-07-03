7ddb53f3f1cad7ad78653a00428df65998f3a34e
Koosha Owji <koosha.owji@gmail.com>
2025-07-03 18:49:13 +1000

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { withAuth } from "@kinde-oss/kinde-auth-nextjs/middleware";
 
 export default withAuth(async function middleware() {}, {
-  publicPaths: ["/api/auth", /^(?!\/dashboard($|\/)).*$/],
+  publicPaths: [/^(?!\/dashboard($|\/)).*$/],
 });
 
 export const config = {
